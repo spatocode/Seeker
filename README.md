dabbdbb8bf2d54b16ebd4597572c6d3b528708b9
Ekene Izukanne <ekeneizukanne@gmail.com>
2017-09-05 07:04:30 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Seeker
+
+Seeker is a networking program used for monitoring Internet and Local Area 
+Network (LAN) activities. It's also capable of capturing and analyzing network
+packets. It gathers information about the data passing through your 
+Network Interface Card (NIC) and decodes the analyzed data.
---
+++ setup.py
@@ -0,0 +1,11 @@
+NAME = "Seeker"
+
+VERSION = "0.0.0alpha1"
+
+COPYRIGHT = "Copyright (c) 2017 Ekene Izukanne <ekeneizukanne@gmail.com>"
+
+DESCRIPTION = """
+    Seeker is a networking program used for monitoring Internet and Local Area 
+    Network (LAN) activities. It's also capable of capturing and analyzing network
+    packets. It gathers information about the data passing through your 
+    Network Interface Card and decodes the analyzed data."""
